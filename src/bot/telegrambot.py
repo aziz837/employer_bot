a011de3124da836b0810f8ec1dc2d46094b05d25
aziz837 <mubashirov2002@gmail.com>
2021-01-26 21:01:41 +0500

    start tayyor

--- src/bot/telegrambot.py
+++ src/bot/telegrambot.py
@@ -1,13 +1,31 @@
 from telegram.ext import CommandHandler, MessageHandler, Filters
 from django_telegrambot.apps import DjangoTelegramBot
+from telegram.ext import (
+    Updater,
+    CallbackContext,
+)
+from telegram import(
+    ReplyKeyboardMarkup,
+    ReplyKeyboardRemove, 
+    Update,  
+    InlineKeyboardButton, 
+    InlineKeyboardMarkup
+) 
 
 import logging
 logger = logging.getLogger(__name__)
 
-def start(update, context):
-    print('salom')
-    update.message.reply_text('Hi!')
+def start(update: Update, context: CallbackContext):
+    keyboard = [
+        [
+            InlineKeyboardButton("Ish beruvchi", callback_data='1'),
+            InlineKeyboardButton("Ish Oluvchi", callback_data='2'),
+        ],
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
 
+    update.message.reply_text('Categoryadan birini Tanlang:', reply_markup=reply_markup)
+    return 
 
 def help_command(update, context):
     update.message.reply_text('Help!')
