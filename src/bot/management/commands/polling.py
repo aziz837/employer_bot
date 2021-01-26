75ea0a3c8068448e677513d64ea89bc4c225cc3d
aziz837 <mubashirov2002@gmail.com>
2021-01-26 21:25:23 +0500

    .env

--- src/bot/management/commands/polling.py
+++ src/bot/management/commands/polling.py
@@ -1,10 +1,11 @@
 import logging
-
+from django.conf import settings
 from django.core.management.base import BaseCommand
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 
 from ...telegrambot import start, help_command
+
 
 class Command(BaseCommand):
     help = "Run telegram bot in polling mode"
@@ -12,7 +13,7 @@ class Command(BaseCommand):
 
     
     def handle(self, *args, **options):
-        updater = Updater("1547973611:AAE3E5orFqkqw_9csRaK2Rqyt9pdCJH1Fh8", use_context=True)
+        updater = Updater(settings.TOKEN_KEY, use_context=True)
         dispatcher = updater.dispatcher
         dispatcher.add_handler(CommandHandler("start", start))
         dispatcher.add_handler(CommandHandler("help", help_command))
