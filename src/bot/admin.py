754c99bacab5915f5200bd6d879b5919b28ac1ae
abdulaziz112005 <abdulazizaziz7777@gmail.com>
2021-01-28 17:15:34 +0500

    added

--- src/bot/admin.py
+++ src/bot/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 # from .forms import ProfileForm
-from .models import Category, Region
+from .models import Category, Region, UserCategory
 
 @admin.register(Category)
 class CategoryAdmin (admin.ModelAdmin):
@@ -10,4 +10,6 @@ class CategoryAdmin (admin.ModelAdmin):
 @admin.register(Region)
 class RegionAdmin (admin.ModelAdmin):
     list_display=('id', 'title', 'parend_id')
-    
+@admin.register(UserCategory)
+class UserCategoryAdmin (admin.ModelAdmin):
+    list_display=('id', 'user_id')
